1664a83bea670b084a4800b02fec4b836c7e3a99
dickgroenenberg <dickgroenenberg@users.noreply.github.com>
2021-11-23 11:50:42 +0100

    NGSpeciesID on batch of fastq's

---
+++ scripts/ngspid.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# requirement: NGSpeciesID and accompanying conda env
+# https://github.com/ksahlin/NGSpeciesID
+
+# eval statement prevents conda activate from erroring out
+eval "$(conda shell.bash hook)"
+conda activate NGSpeciesID
+
+for i in *.fastq
+do
+    NGSpeciesID --ont --fastq $i --outfolder "$i".out --consensus --medaka
+done
+
+conda deactivate
